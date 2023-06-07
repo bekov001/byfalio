dca76b382d747cd1d7595827b28dbf47433c94f7
Anuar <ava200ava@gmail.com>
2023-06-07 15:47:58 +0600

    orogin main jahj

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -11,7 +11,7 @@ import os
 from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '.settings')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
