7c0be8ddf78f09e629292abfabb10231bcf31eba
Anuar <ava200ava@gmail.com>
2023-06-07 16:05:03 +0600

    orogin main jahj

--- server/server/settings.py
+++ server/server/settings.py
@@ -66,11 +66,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-'whitenoise.middleware.WhiteNoiseMiddleware',
+# 'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'server.urls'
 
