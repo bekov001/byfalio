ae80c4b6b3522ae6c4ad00cd9807ddacafebb44a
Anuar <ava200ava@gmail.com>
2023-07-15 22:19:15 +0600

    ProcFile

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -13,18 +13,18 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-import django
+
+
+django_asgi_app = get_asgi_application()
+
 from exchange import routing
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 
-django.setup()
-
-# application = get_asgi_application()
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app(),
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
     ),
--- server/server/settings.py
+++ server/server/settings.py
@@ -174,7 +174,7 @@ CHANNEL_LAYERS = {
         # "BACKEND": "channels.layers.InMemoryChannelLayer"
          "BACKEND": "channels_redis.core.RedisChannelLayer",
             "CONFIG": {
-                "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
+                "hosts": [("redis-server-name", 6379)],
             },
     },
 
