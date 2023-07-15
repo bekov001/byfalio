5c6dc165c6164753e8f6d3cdd47f46db2f19c1b9
Anuar <ava200ava@gmail.com>
2023-07-15 22:08:18 +0600

    ProcFile

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -13,10 +13,13 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
+import django
 from exchange import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+
+
+django.setup()
 
 # application = get_asgi_application()
 
