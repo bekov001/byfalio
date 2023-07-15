bec549defb7e72ce4d99f2800961304c2445312e
Anuar <ava200ava@gmail.com>
2023-07-15 21:12:22 +0600

    ProcFile

--- server/server/settings.py
+++ server/server/settings.py
@@ -174,7 +174,7 @@ CHANNEL_LAYERS = {
         # "BACKEND": "channels.layers.InMemoryChannelLayer"
          "BACKEND": "channels_redis.core.RedisChannelLayer",
             "CONFIG": {
-                "hosts": [("localhost", 6379)],
+                "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
             },
     },
 
